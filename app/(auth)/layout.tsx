90c3f2a4a91d796b023134cce3e860c6bdf435e1
Zakia Mangal <zakiaimran513@gmail.com>
2025-04-16 20:18:36 +0430

    add register and login screens

---
+++ app/(auth)/layout.tsx
@@ -0,0 +1,20 @@
+'use client';
+import React from 'react';
+import '../globals.css';
+
+export default function DashboardLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <html lang='en' className='h-full w-full'>
+      <body className='h-full'>
+        <div className='flex flex-col min-h-screen bg-white'>
+          {/* Main content area with increased bottom padding to accommodate taller navbar */}
+          <main className='flex-1 pb-24'>{children}</main>
+        </div>
+      </body>
+    </html>
+  );
+}
